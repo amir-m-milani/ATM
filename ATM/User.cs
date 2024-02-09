8f5d38c35eed28e1da62e84ac5f69de14c792a7d
amir.m.milani <amirmilani08@gmail.com>
2024-02-09 21:48:59 +0330

    make user functionality for checking card and password and balance if needed

--- ATM/User.cs
+++ ATM/User.cs
@@ -1,18 +1,64 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text.Json;
+
 namespace ATM;
 
 class User
 {
-    int id;
-    string name;
-    string cardNO;
-    public User(
-        int id,
-        string name,
-        string cardNO
-    )
+    private Dictionary<string, string> users = [];
+    private string cardNO;
+    private string password;
+    private float balance;
+    public User(string cardNO, string password, float balance)
     {
-        this.id = id;
-        this.name = name;
         this.cardNO = cardNO;
+        this.password = password;
+        this.balance = balance;
     }
+    /// <summary>
+    /// returns true if user does not exists
+    /// </summary>
+    /// <returns>bool</returns>
+    public bool AddUser()
+    {
+        if (!this.users.ContainsKey(this.cardNO))
+            return false;
+        this.users.Add(this.cardNO, this.password);
+        return true;
+    }
+    /// <summary>
+    /// check if the user not exits return false;
+    /// check if the password not match return false;
+    /// </summary>
+    /// <returns>bool</returns>
+    public bool checkCardnumberAndPassword()
+    {
+        if (!this.users.ContainsKey(this.cardNO))
+            return false;
+        if (this.users[this.cardNO] != this.password)
+            return false;
+        return true;
+    }
+    /// <summary>
+    /// if user not exists return false 
+    /// </summary>
+    /// <returns>bool</returns>
+    public bool checkCardnumber()
+    {
+        if (!this.users.ContainsKey(this.cardNO))
+            return false;
+        return true;
+    }
+    /*
+    public void SaveUserToFile(string cardNumber, string password)
+    {
+        User user = new(cardNumber, password);
+        string json = JsonSerializer.Serialize(user); // serialize the object to a JSON string
+        File.WriteAllText("users.json", json); // write the JSON string to a file
+        System.Console.WriteLine("its done!");
+    }
+    */
+
+
 }
